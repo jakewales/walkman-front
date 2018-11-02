b83a4fb5b2fba455097ea2952c1348b72d6793e7
JingeXC <cbb467cp01@163.com>
2018-11-02 18:41:24 +0800

    测试上传文件api

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -28,19 +28,45 @@ const init = async() => {
     password: '123'
   });
   sessionStorage.setItem('token', response.data.token);
-  const token = sessionStorage.getItem('token');
-  if (token) {
-    const instance = axios.create({
-      responseType: 'arraybuffer',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    const musicResponse = await instance.get('http://localhost:3000/audio/1');
-    const audioInstance = new AudioComponent(musicResponse.data);
-    const canvasAnimation = new CanvasComponent(canvasElement, audioInstance.init(), 'wave');
-  }
+  // const token = sessionStorage.getItem('token');
+  // if (token) {
+  //   const instance = axios.create({
+  //     responseType: 'arraybuffer',
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`
+  //     }
+  //   });
+  //   const musicResponse = await instance.get('http://localhost:3000/audio/1');
+  //   const audioInstance = new AudioComponent(musicResponse.data);
+  //   const canvasAnimation = new CanvasComponent(canvasElement, audioInstance.init(), 'wave');
+  // }
 };
 
 init();
+
+// upload files
+
+const uploadInputElement = <HTMLInputElement> document.querySelector('#uploadFile');
+const uploadButtonElement = <HTMLButtonElement> document.querySelector('#uploadButton');
+
+let uploadInput = new InputComponent(uploadInputElement, 'audio');
+
+uploadButtonElement.onclick = async function() {
+  const token = sessionStorage.getItem('token');
+
+  let formData = new FormData();
+  formData.append('audio', uploadInput.rawFiles[0]);
+
+  if (token) {
+    const requestInstance = axios.create({
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    let response = await requestInstance.post('http://localhost:3000/uploadAudio', formData);
+    console.log(response);
+  }
+}
+
 
